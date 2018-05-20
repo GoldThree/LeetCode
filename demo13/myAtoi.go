20ee6909c40c8ea3f536e75ad8577d31e12a3aff
duan <xin.duan@echocloud.com>
2018-05-20 14:45:30 +0800

    refactor: modify code

--- demo13/myAtoi.go
+++ demo13/myAtoi.go
@@ -1,81 +1,47 @@
 package main
 
 import (
-	"strings"
-	"math"
 	"fmt"
 )
 
 func main() {
-	result := myAtoi("0903we4wewe")
+	result := myAtoi("10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000522545459")
 	fmt.Println(result)
 }
 func myAtoi(str string) int {
-	// 除去 str 首尾的空格
-	s := strings.TrimSpace(str)
-	if len(s) == 0 {
+	if len(str) == 0 {
 		return 0
 	}
-
-	// 取出 s 的符号和主体 x
-	sign, x := getSign(s)
-
-	// 裁剪x丢弃混入的非数字字符
-	x = trim(x)
-
-	// 根据sigh和x，转换成int
-	return convert(sign, x)
-}
-
-func getSign(s string) (int, string) {
-	sign := 1
-	switch s[0] {
-	case '-':
-		s = s[1:]
-		sign = -1.0
-	case '+':
-		s = s[1:]
-	default:
-	}
-
-	return sign, s
-}
-
-func trim(s string) string {
-	for i := range s {
-		if s[i] < '0' || '9' < s[i] {
-			return s[:i]
+	var (
+		num int
+		neg bool
+	)
+	for i, s := range str {
+		if s != ' ' {
+			str = str[i:]
+			break
 		}
 	}
-
-	return s
-}
-
-func convert(sign int, s string) int {
-	base := 1 * sign
-	res := 0
-	yes := false
-
-	for i := len(s) - 1; i >= 0; i-- {
-		// 为了防止特别长的数字，甚至超过float64的范围，所以，每一步都检查是否溢出
-		res, yes = isOverflow(res + (int(s[i])-48)*base)
-		fmt.Println(res, i,s[i])
-		if yes {
-			return res
+	if str[0] == '+' {
+		str = str[1:]
+	} else if str[0] == '-' {
+		str = str[1:]
+		neg = true
+	}
+	for _, s := range str {
+		if s < '0' || s > '9' {
+			break
 		}
-		base *= 10
+		num = num*10 + int(s-'0')
+		if num > 2147483648 {
+			num = 2147483648
+			break
+		}
 	}
-
-	return res
-}
-
-func isOverflow(i int) (int, bool) {
-	switch {
-	case i > math.MaxInt32:
-		return math.MaxInt32, true
-	case i < math.MinInt32:
-		return math.MinInt32, true
-	default:
-		return i, false
+	if neg {
+		num *= -1
+	} else if num > 2147483647 {
+		num = 2147483647
 	}
+	return num
 }
