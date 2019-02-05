f614fb47fc46092900604903106026c91944a4bc
duan <xin.duan@echocloud.com>
2019-02-05 21:47:51 +0800

    feat: add No 101 in leetcode

---
+++ No101SymmetricTree/SymmetricTree.go
@@ -0,0 +1,27 @@
+package No101SymmetricTree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return recur(root.Left, root.Right)
+}
+
+func recur(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+
+	if left == nil || right == nil {
+		return false
+	}
+
+	return left.Val == right.Val && recur(left.Left, right.Right) && recur(left.Right, right.Left)
+}
