8005cb55ba9f66d93c097bfec5294c2cc277952b
duan <xin.duan@echocloud.com>
2019-02-05 21:09:38 +0800

    feat: add NO 100 in leetcode

---
+++ NO100SameTree/sameTree.go
@@ -0,0 +1,30 @@
+package NO100SameTree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if (p == nil && q != nil) || (p != nil && q == nil) {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	isLeftSame := isSameTree(p.Left, q.Left)
+	if !isLeftSame {
+		return false
+	}
+
+	isRightSame := isSameTree(p.Right, q.Right)
+	if !isRightSame {
+		return false
+	}
+	return true
+
+}
