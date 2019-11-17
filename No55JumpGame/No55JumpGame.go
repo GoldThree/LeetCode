9386177577bb9dd1354697cb89f93eaaba8f3d9a
GoldThree <37068218+GoldThree@users.noreply.github.com>
2019-11-17 15:37:47 +0800

    feat(): add no 55 jump game (#62)

---
+++ No55JumpGame/No55JumpGame.go
@@ -0,0 +1,25 @@
+package No55JumpGame
+
+func canJump(nums []int) bool {
+	if len(nums) == 0 {
+		return false
+	}
+	step := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if step > 0 {
+			step--
+			step = max(step, nums[i])
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+func max(i, j int) int {
+	if i >= j {
+		return i
+	}
+	return j
+}
+
